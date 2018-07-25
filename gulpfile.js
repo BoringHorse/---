5f27b83698418a270ae15cb1a3a9a4593e7dbfa4
Nogtkov <annogotkov@yandex.ru>
2018-07-25 21:11:46 +0300

    test commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	concat = require('gulp-concat-css'),
+	less = require('gulp-less'),
+	minify = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	prefixer = require('gulp-autoprefixer');
+
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		},
+		notify: false
+	});
+
+	gulp.watch('app/less/*.less', ['less']);
+	gulp.watch('app/index.html').on('change', browserSync.reload);
+});
+
+gulp.task('less', function() {
+	console.log('Hello, I have runned!');
+	gulp.src('app/less/*.less')
+	.pipe(less())
+	.pipe(prefixer({
+		browsers: ["last 2 version", " > ie10"],
+		cascade: true
+	}))
+	.pipe(minify())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['server']);
